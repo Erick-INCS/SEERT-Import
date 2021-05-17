db07915173b0e89e42076b0afc936df795a6f1cf
Erick Arroyo <erick.arrollo@grsoluciones.net>
2021-05-17 09:34:02 -0700

    fix text format

--- main.scala
+++ main.scala
@@ -58,6 +58,10 @@ def saveBatch(name:String, batchSize:Int, content:org.apache.spark.sql.Dataset[S
   }
 }
 
+def clearValue(value:String) : String = {
+  value.replace("'", "''").replace("\r", "\\r").replace("\n", "\\n")
+}
+
 registerDFList(Seq(
   (Connections.fb, "PRODUCTOS"),
   (Connections.mssql, "GR_PRODUCTOS_fix_NPARTE")
@@ -84,17 +88,17 @@ saveBatch("result.sql",
         P_GENDESP,
         P_GENMERMA
     ) VALUES (
-      ${if (row.getAs[String]("NPArte") == null) "NULL" else s"'${row.getAs[String]("NPArte")}'"},
-      ${if (row.getAs[String]("DescEsp") == null) "NULL" else s"'${row.getAs[String]("DescEsp")}'"},
-      ${if (row.getAs[String]("DescIng") == null) "NULL" else s"'${row.getAs[String]("DescIng")}'"},
+      ${if (row.getAs[String]("NPArte") == null) "NULL" else s"'${row.getAs[String]("NPArte").replace("'", "''").replace("\r", "\\r").replace("\n", "\\n")}'"},
+      ${if (row.getAs[String]("DescEsp") == null) "NULL" else s"'${row.getAs[String]("DescEsp").replace("'", "''").replace("\r", "\\r").replace("\n", "\\n")}'"},
+      ${if (row.getAs[String]("DescIng") == null) "NULL" else s"'${row.getAs[String]("DescIng").replace("'", "''").replace("\r", "\\r").replace("\n", "\\n")}'"},
       ${if (row.getAs[String]("Tipo") == null) "NULL" else s"'${row.getAs[String]("Tipo")}'"},
       ${if (row.getAs[String]("TipoParte") == null) "NULL" else s"'${row.getAs[String]("TipoParte")}'"},
-      ${if (row.getAs[String]("UM") == null) "NULL" else s"'${row.getAs[String]("UM")}'"},
-      ${if (row.getAs[String]("UMedidaFraccion") == null) "NULL" else s"'${row.getAs[String]("UMedidaFraccion")}'"},
-      ${if (row.getAs[String]("FracMex") == null) "NULL" else s"'${row.getAs[String]("FracMex")}'"},
-      ${if (row.getAs[String]("ArancelUs") == null) "NULL" else s"'${row.getAs[String]("ArancelUs")}'"},
+      ${if (row.getAs[String]("UM") == null) "NULL" else s"'${row.getAs[String]("UM").replace("'", "''").replace("\r", "\\r").replace("\n", "\\n").toUpperCase}'"},
+      ${if (row.getAs[String]("UMedidaFraccion") == null) "NULL" else s"'${row.getAs[String]("UMedidaFraccion").replace("'", "''").replace("\r", "\\r").replace("\n", "\\n").toUpperCase}'"},
+      ${if (row.getAs[String]("FracMex") == null) "NULL" else s"'${row.getAs[String]("FracMex").replace("'", "''").replace("\r", "\\r").replace("\n", "\\n")}'"},
+      ${if (row.getAs[String]("ArancelUs") == null) "NULL" else s"'${row.getAs[String]("ArancelUs").replace("'", "''").replace("\r", "\\r").replace("\n", "\\n")}'"},
       ${if (row.getAs[String]("FactConv") == null) "NULL" else s"'${row.getAs[String]("FactConv")}'"},
-      ${if (row.getAs[String]("Nota") == null) "NULL" else s"'${row.getAs[String]("Nota")}'"},
+      ${if (row.getAs[String]("Nota") == null) "NULL" else s"'${row.getAs[String]("Nota").replace("'", "''").replace("\r", "\\r").replace("\n", "\\n")}'"},
       ${if (row.getAs[String]("Desp") == null) "NULL" else s"'${row.getAs[String]("Desp")}'"},
       ${if (row.getAs[String]("Merm") == null) "NULL" else s"'${row.getAs[String]("Merm")}'"}
     );\n"""
---
+++ pack.sh
@@ -0,0 +1,7 @@
+cd data
+rm run.bat
+for i in $(ls *.sql); do
+    echo isql -i $i 192.168.1.148:\"/grsc/Clientes/ClientesGRSA/Rockwell Automation Monterrey/Base Datos 2020/Rockwell-UPD-31Dic-2020.fdb\" -user SYSDBA -pass masterkey >> run.bat
+done;
+zip -r data *.sql *.bat
+rm *.sql
