eb0c17124885f6364521c09dbd896c59a2b29414
Erick Arroyo <incspnsoft@gmail.com>
2021-05-18 21:07:33 -0700

    Auto SQL (in and out) added.

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
+mkdir -p data
 spark-shell --driver-class-path drivers/mssql-jdbc-9.2.1.jre11.jar,drivers/jaybird-full-3.0.9.jar --jars drivers/mssql-jdbc-9.2.1.jre11.jar,drivers/jaybird-full-3.0.9.jar
