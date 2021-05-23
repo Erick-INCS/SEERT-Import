984784a423bead97b421a997e871e61c7c7135d1
Erick Arroyo <erick.arrollo@grsoluciones.net>
2021-05-23 07:17:04 -0700

    last version

---
+++ pack.bat
@@ -0,0 +1,10 @@
+cd data
+rm run.bat
+for i in $(ls *.sql); do
+    echo isql -i $i 192.168.1.148:\"/grsc/Clientes/ClientesGRSA/Rockwell Automation Monterrey/Base de datos 2021/Migracion de BD/ROCKWELL_MIG.FDB\" -user SYSDBA -pass masterkey >> run.bat
+done;
+zip -r data *.sql *.bat
+rm *.sql
+cd data
+scp data.zip Erick@192.168.1.179:Documents/
+cd ..
--- pack.sh
+++ pack.sh
@@ -1,10 +1,11 @@
 cd data
-rm run.bat
+rm run.sh run.bat
 for i in $(ls *.sql); do
-    echo isql -i $i 192.168.1.148:\"/grsc/Clientes/ClientesGRSA/Rockwell Automation Monterrey/Base de datos 2021/Migracion de BD/ROCKWELL_MIG.FDB\" -user SYSDBA -pass masterkey >> run.bat
+    echo isql -i $i 192.168.1.148:\"/grsc/Clientes/ClientesGRSA/Rockwell Automation Monterrey/Base de datos 2021/Migracion de BD/ROCKWELL_MIG.FDB\" -user SYSDBA -pass masterkey >> run.sh
 done;
-zip -r data *.sql *.bat
-rm *.sql
-cd data
-scp data.zip Erick@192.168.1.179:Documents/
-cd ..
+./run.sh
+# zip -r data *.sql *.bat
+# rm *.sql
+# cd data
+# scp data.zip Erick@192.168.1.179:Documents/
+# cd ..
